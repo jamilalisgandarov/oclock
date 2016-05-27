1dfc4945dcc5c4d91d21b05fb7a50b19eca64dd7
jamilalisgandarov <jamil.e@code.edu.az>
2016-05-27 13:25:14 +0400

    Clock CSS , JavaScript and HTML structure are updated and completed

--- js/main.js
+++ js/main.js
@@ -11,13 +11,24 @@
     	            divLarge.style.zIndex = "2";
     	            var degreeL = 6 * i;
     	            divLarge.style.transform = 'rotate(' + degreeL + 'deg)'
-    	            console.log(i)
-
     	        }
     	        divLarge.setAttribute("class", "divLarge");
     	        var degree = 6 * i;
     	        divs.setAttribute("class", "rad1");
-    	        divs.style.transform = 'rotate(' + degree + 'deg)'
+    	        divs.style.transform = 'rotate(' + degree + 'deg)';
+
     	    }
     	}
     	creator();
+
+    	function time() {
+    	    var d = new Date();
+    	    var seconds = d.getSeconds();
+    	    var mins = d.getMinutes();
+    	    var hours = d.getHours();
+    	    document.getElementsByClassName("crossSecond")[0].style.transform = 'rotate(' + seconds * 6 + 'deg)';
+    	    document.getElementsByClassName("crossMin")[0].style.transform = 'rotate(' + mins * 6 + 'deg)';
+    	    document.getElementsByClassName("crossHours")[0].style.transform = 'rotate(' + (hours * 30 + mins * 0.5) + 'deg)';
+    	    console.log(document.getElementsByClassName("crossSecond")[0].style.transform);
+    	}
+    	var date = setInterval(function() { time() }, 1000);
